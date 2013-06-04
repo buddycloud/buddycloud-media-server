831ce609998e37ddbb49318505770005c5eea6dc
Lloyd Watkin <lloyd.watkin@surevine.com>
2013-06-04 10:51:05 +0000

    Update where classes are pulled from

--- run.sh
+++ run.sh
@@ -4,7 +4,11 @@ VERSION=`grep '<version' pom.xml | cut -f2 -d">"|cut -f1 -d"<"|head -n1`
 GROUP_ID=`grep '<groupId' pom.xml | cut -f2 -d">"|cut -f1 -d"<"|head -n1`
 NAME=`grep '<name' pom.xml | cut -f2 -d">"|cut -f1 -d"<"|head -n1`
 
+cd target
+unzip $NAME-$VERSION.zip
+cd ..
+
 CLASSPATH=""
-for f in `find target/$NAME-$VERSION/$NAME/lib/ -name '*jar'`; do CLASSPATH=$CLASSPATH":"$f; done
+for f in `find target/$NAME/lib/ -name '*jar'`; do CLASSPATH=$CLASSPATH":"$f; done
 
 java -cp target/$NAME-$VERSION.jar$CLASSPATH $GROUP_ID.Main
