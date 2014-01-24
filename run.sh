d6204a910999d8bb04fe69f75b6b20916d939ce6
rodrigods <rodrigodsousa@gmail.com>
2014-01-24 10:21:57 +0100

    Adding tar.gz and tar.bz2 tarballs

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 mvn clean package
 
 VERSION=`grep '<version' pom.xml | cut -f2 -d">"|cut -f1 -d"<"|head -n1`
@@ -6,7 +7,7 @@ NAME=`grep '<name' pom.xml | cut -f2 -d">"|cut -f1 -d"<"|head -n1`
 
 cd target
 unzip $NAME-$VERSION.zip
-cd ..
+cd ..;
 
 CLASSPATH=""
 for f in `find target/$NAME/lib/ -name '*jar'`; do CLASSPATH=$CLASSPATH":"$f; done
